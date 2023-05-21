6f80f2449507f23f4951f36c8e7519a740656da0
Malek Boubakri <malekbouba@gmail.com>
2023-05-21 04:08:40 +0100

    chat plugin with ref & no init

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,54 +1,26 @@
-import React, { FC, useEffect } from "react";
-import { getNavigatorLanguage, ISOLangCountry } from "../locales";
+import React, { FC, useEffect, useRef } from "react";
 
 export interface ChatProps {
-  appId?: string;
   pageId: string;
-  cookie?: boolean;
-  language?: ISOLangCountry;
 }
 
-const Chat: FC<ChatProps> = ({ appId, pageId, cookie, language }) => {
+const Chat: FC<ChatProps> = ({ pageId }) => {
+  const container = useRef<HTMLDivElement>(null);
+
   useEffect(() => {
-    if (typeof window !== "undefined" && pageId) {
-      const chatbox = document.getElementById("fb-customer-chat");
-      if (chatbox) {
-        chatbox.setAttribute("page_id", pageId);
-        chatbox.setAttribute("attribution", "biz_inbox");
-      }
-      // Define the FB async init function
-      window.fbAsyncInit = () => {
-        window.FB.init({
-          appId,
-          autoLogAppEvents: true,
-          xfbml: true,
-          version: "v16.0",
-        });
-      };
-      // Load the Facebook SDK asynchronously
-      let js: HTMLScriptElement;
-      const fjs = document.getElementsByTagName("script")[0];
-      if (!document.getElementById("facebook-jssdk")) {
-        js = document.createElement("script");
-        js.id = "facebook-jssdk";
-        js.async = true;
-        js.defer = true;
-        js.crossOrigin = "anonymous";
-        js.src = `https://connect.facebook.net/${
-          language || getNavigatorLanguage()
-        }/sdk/xfbml.customerchat.js`;
-        js.nonce = "aieR2yIx";
-        fjs?.parentNode?.insertBefore(js, fjs);
-      }
-    } else {
-      console.error("props 'appId' is required to initiate Facebook SDK!");
+    if (typeof window !== "undefined" && container.current) {
+      container.current.setAttribute("page_id", pageId);
+      container.current.setAttribute("attribution", "biz_inbox");
     }
-  }, [appId, pageId, cookie, language]);
+  }, [container.current]);
 
   return (
     <>
-      <div id="fb-root" />
-      <div id="fb-customer-chat" className="fb-customerchat"></div>
+      <div
+        ref={container}
+        id="fb-customer-chat"
+        className="fb-customerchat"
+      ></div>
     </>
   );
 };
